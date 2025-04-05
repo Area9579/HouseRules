deb343a3f3d1340b4ed01726be78a6f32a94bfc3
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-04-05 15:23:29 -0500

    added mercy timer

--- house-rules/Scripts/keyIndicator.gd
+++ house-rules/Scripts/keyIndicator.gd
@@ -1,23 +1,29 @@
 extends Node3D
 
 @onready var keyLabel: Label3D = $Label3D
+@onready var timerLabel: Label3D = $TimerLabel
 
 @export var key: String
 @export_range(0, 5, 0.5) var mercyTimer: float = 0.5
 
 var currentMercyTimer = 0
+var releasedKey: bool = true
 
 func _ready() -> void:
 	Director.keyPressed.connect(keyIsVisible)
-	Director.keyReleased.connect(keyIsNotVisible)
+	Director.keyReleased.connect(releaseKey)
 	keyLabel.text = key
+	timerLabel.text = str(mercyTimer)
 
 
 func _process(delta: float) -> void:
-	if currentMercyTimer > 0:
-		currentMercyTimer -= delta
-	else:
-		pass
+	if releasedKey:
+		if currentMercyTimer > 0:
+			currentMercyTimer -= delta
+		else:
+			keyIsNotVisible()
+	
+	updateTimerLabel()
 
 
 
@@ -30,9 +36,21 @@ func _input(event: InputEvent) -> void:
 
 func keyIsVisible(signalKey):
 	if signalKey == key:
+		releasedKey = false
+		currentMercyTimer = mercyTimer
 		keyLabel.visible = true
+		timerLabel.visible = true
 
 
-func keyIsNotVisible(signalKey):
+func keyIsNotVisible():
+	keyLabel.visible = false
+	timerLabel.visible = false
+
+
+func releaseKey(signalKey):
 	if signalKey == key:
-		keyLabel.visible = false
+		releasedKey = true
+
+
+func updateTimerLabel():
+	timerLabel.text = str(int(currentMercyTimer * 100) / 100.0)
