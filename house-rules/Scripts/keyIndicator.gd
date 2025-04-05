dab7eea9630cb9e7a619d0f0251e97988b6d37c3
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-04-05 15:33:54 -0500

    Comments and some renaming

--- house-rules/Scripts/keyIndicator.gd
+++ house-rules/Scripts/keyIndicator.gd
@@ -10,47 +10,54 @@ var currentMercyTimer = 0
 var releasedKey: bool = true
 
 func _ready() -> void:
-	Director.keyPressed.connect(keyIsVisible)
-	Director.keyReleased.connect(releaseKey)
+	#connect both of the signals for pressing and releasing keys
+	Director.keyPressed.connect(keyIsPressed)
+	Director.keyReleased.connect(keyIsReleased)
+	#both of these are setting the text for the indicators, this will prob be changed later
 	keyLabel.text = key
 	timerLabel.text = str(mercyTimer)
 
 
 func _process(delta: float) -> void:
+	#if a key is released, then decrease the timer
 	if releasedKey:
 		if currentMercyTimer > 0:
 			currentMercyTimer -= delta
 		else:
-			keyIsNotVisible()
+			setLabelsInvisible()
 	
 	updateTimerLabel()
 
 
 
 func _input(event: InputEvent) -> void:
+	#Check to see if the button you pressed is equivalent to the key variable, then send a signal to director
 	if event is InputEventKey and event.is_pressed() and key == OS.get_keycode_string(event.get_keycode_with_modifiers()):
 		Director.keyPressed.emit(OS.get_keycode_string(event.get_keycode_with_modifiers()))
+	#Check to see if the key you released is equivalent to the key variable, then send a signal to director
 	elif event is InputEventKey and key == OS.get_keycode_string(event.get_keycode_with_modifiers()):
 		Director.keyReleased.emit(OS.get_keycode_string(event.get_keycode_with_modifiers()))
 
 
-func keyIsVisible(signalKey):
+func keyIsPressed(signalKey):
+	#resets the timer, and turns the labels to be visible when key is pressed
 	if signalKey == key:
-		releasedKey = false
 		currentMercyTimer = mercyTimer
+		releasedKey = false
 		keyLabel.visible = true
 		timerLabel.visible = true
 
 
-func keyIsNotVisible():
-	keyLabel.visible = false
-	timerLabel.visible = false
-
-
-func releaseKey(signalKey):
+func keyIsReleased(signalKey):
+	#change bool to state that a key has been released
 	if signalKey == key:
 		releasedKey = true
 
 
-func updateTimerLabel():
+func setLabelsInvisible(): #ok this one is pretty self explanatory
+	keyLabel.visible = false
+	timerLabel.visible = false
+
+
+func updateTimerLabel(): #yeah I don't need to say much about this, just formatting
 	timerLabel.text = str(int(currentMercyTimer * 100) / 100.0)
