b53d4cd8d4ef8b771f41be22368e9ef54326afd0
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-04-05 13:19:20 -0500

    Added Player Camera Movement

---
+++ house-rules/Scripts/camera.gd
@@ -0,0 +1,33 @@
+extends Camera3D
+
+@export var mouseSensitivity: int 
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(_delta):
+	rotation_degrees.x = clamp(rotation_degrees.x, -30, 30)
+	rotation_degrees.y = clamp(rotation_degrees.y, -160, -20)
+	rotation_degrees.z = clamp(rotation_degrees.z, 0, 0)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("RightClick"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event is InputEventMouseMotion:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+		#uncomment this to try the wierd camera movement
+		
+		#if event.relative.x > 0:
+			#rotate_y(3 * -0.005 * mouseSensitivity)
+		#elif event.relative.x < 0:
+			#rotate_y(3 * 0.005 * mouseSensitivity)
+		#if event.relative.y > 0:
+			#rotate_z(3 * -0.005 * mouseSensitivity)
+		#elif event.relative.y < 0:
+			#rotate_z(3 * 0.005 * mouseSensitivity)
+		
+		#comment this to try the other movement
+		rotate_y(clampf(event.relative.x, -30, 30) * -0.005 * mouseSensitivity)
+		rotate_z(clampf(event.relative.y, -30, 30) * -0.005 * mouseSensitivity)
+	
