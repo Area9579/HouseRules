434135c3093549a4d8ad9360891a9b0fdf93ad6b
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2025-04-05 02:31:09 -0500

    BaseGame

--- house-rules/Scripts/base_game_logic.gd
+++ house-rules/Scripts/base_game_logic.gd
@@ -4,11 +4,21 @@ extends Node3D
 @onready var y: Label3D = $Rows/Y
 @onready var z: Label3D = $Rows/Z
 
+var matrix = [["00","01","02"],["10","11","12"],["20","21","22"]]
+
 func _ready() -> void:
-	var matrix = [["00","01","02"],["10","11","12"],["20","21","22"]]
 	
-	print(matrix[0][2])
+	update_board_scores()
+
+func update_board_scores():
+	var row_x = 0
+	var row_y = 0
+	var row_z = 0
 	
+	#group up multipliers
+	for i in 3:
+		print(matrix[i])
+		for u in matrix[i]:
+			pass
 	
-	
-	
+	#multiply
