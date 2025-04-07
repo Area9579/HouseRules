35c20b2ec3afa6fd9e2e496ad5a3298e90fa34cd
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-04-07 15:30:14 -0500

    Minor changes to speech box

--- house-rules/Scripts/speech_text.gd
+++ house-rules/Scripts/speech_text.gd
@@ -8,20 +8,15 @@ extends Node3D
 var string : String = "The quick brown fox jumped over the lazy dog..."
 
 
-func _ready() -> void:
-	pass
-	#tweenText(string)
-
-
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("right"):
 		tweenText(string)
-	subViewport.size = richTextLabel.size
+	subViewport.size = Vector2(richTextLabel.size.x,richTextLabel.size.y)
 	richTextLabel.position = Vector2.ZERO
 	
 
 func tweenText(text: String):
-	richTextLabel.text = text
+	richTextLabel.text = "[shake rate=20.0 level=5 connected=1]" + text + "[/shake]"
 	var tween = get_tree().create_tween()
 	richTextLabel.visible_ratio = 0
 	tween.tween_property(richTextLabel, "visible_ratio", 1.0, 5.0)
